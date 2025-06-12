edd8451ab11e9689087f94f4a85f6f5c480ae24e
Schiktor <Meremkulov.marat@yandex.ru>
2025-06-13 00:01:26 +0300

    removing a function generateRandomColor

--- app/main.js
+++ app/main.js
@@ -20,16 +20,6 @@ if(type === 'lock') {
 
 const  cols = document.querySelectorAll('.col')
 
-function generateRandomColor () {
-    const hexCodes = '0123456789ABCDEF';
-    let color = '';
-    for(let i = 0; i < 6 ; i++) {
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    
-    return '#' + color
-}
-
 function copyToClickboard (text) {
     return navigator.clipboard.writeText(text)
 }
