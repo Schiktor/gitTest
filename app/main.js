2bf23bf443d1fce96d1d4cc0a8c6c29415ef7b46
Schiktor <Meremkulov.marat@yandex.ru>
2025-06-12 22:56:16 +0300

    activating the icon block

--- app/main.js
+++ app/main.js
@@ -1,3 +1,20 @@
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if(event.code.toLocaleLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) => {
+const type = event.target.dataset.type
+if(type === 'lock') {
+    const node = event.target.tagName.toLocaleLowerCase() === 'i'
+    ? event.target
+    :event.target.children[0]
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
+}
+})
 
 const  cols = document.querySelectorAll('.col')
 
@@ -13,9 +30,13 @@ function generateRandomColor () {
 
 function setRandomColors () {
 cols.forEach((col) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
-    const color = generateRandomColor ();
+    const color = chroma.random();
+    if(isLocked) {
+        return
+    }
     text.textContent = color;
     col.style.background= color;
     setShadesColor(text, color);
